4dad4879536aa6275fea7d0743100aff73cfea60
Evan Martin <evan.martin@gmail.com>
2022-09-15 16:35:24 -0700

    split

---
+++ win32/src/lib.rs
@@ -0,0 +1,36 @@
+mod pe;
+mod x86;
+mod reader;
+
+pub use x86::X86;
+
+pub fn load_exe(x86: &mut X86, buf: &[u8]) -> anyhow::Result<()> {
+    let file = pe::parse(&buf)?;
+    println!("{file:#?}");
+
+    let base = file.opt_header.image_base;
+    x86.mem
+        .resize((base + file.opt_header.size_of_image) as usize, 0);
+    println!(
+        "image base {base:#x}, image total size {:#x}",
+        x86.mem.len()
+    );
+    for sec in file.sections.iter() {
+        let src = sec.pointer_to_raw_data as usize;
+        let dst = (base + sec.virtual_address) as usize;
+        let size = sec.size_of_raw_data as usize;
+        println!(
+            "sec {:?} at {dst:#x} size {size:#x} from {src:#x}",
+            sec.name
+        );
+        if !sec
+            .characteristics
+            .contains(pe::ImageSectionFlags::UNINITIALIZED_DATA)
+        {
+            x86.mem[dst..dst + size].copy_from_slice(&buf[src..(src + size)]);
+        }
+    }
+    let entry_point = base + file.opt_header.address_of_entry_point;
+    x86.regs.eip = entry_point;
+    Ok(())
+}
---
+++ win32/src/x86.rs
@@ -0,0 +1,55 @@
+
+pub struct Registers {
+    pub eax: u32,
+    pub ebx: u32,
+    pub ecx: u32,
+    pub edx: u32,
+
+    pub esp: u32,
+    pub ebp: u32,
+    pub esi: u32,
+    pub edi: u32,
+
+    pub eip: u32,
+
+    pub cs: u16,
+    pub ds: u16,
+    pub es: u16,
+    pub fs: u16,
+    pub gs: u16,
+    pub ss: u16,
+}
+impl Registers {
+    fn new() -> Self {
+        Registers {
+            eax: 0,
+            ebx: 0,
+            ecx: 0,
+            edx: 0,
+            esp: 0,
+            ebp: 0,
+            esi: 0,
+            edi: 0,
+            eip: 0,
+            cs: 0,
+            ds: 0,
+            es: 0,
+            fs: 0,
+            gs: 0,
+            ss: 0,
+        }
+    }
+}
+
+pub struct X86 {
+    pub mem: Vec<u8>,
+    pub regs: Registers,
+}
+impl X86 {
+    pub fn new() -> Self {
+        X86 {
+            mem: Vec::new(),
+            regs: Registers::new(),
+        }
+    }
+}
