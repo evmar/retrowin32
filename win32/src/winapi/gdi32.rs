4bd8915eef516e230dc1fdf429739a24e3de97a7
Evan Martin <evan.martin@gmail.com>
2022-10-07 20:17:21 -0700

    stubs

--- win32/src/winapi/gdi32.rs
+++ win32/src/winapi/gdi32.rs
@@ -17,6 +17,15 @@ fn GetStockObject(_x86: &mut X86, _i: u32) -> u32 {
     0
 }
 
+fn SelectObject(_x86: &mut X86, _hdc: u32, _hGdiObj: u32) -> u32 {
+    // TODO: return previous obj
+    0
+}
+
+fn GetObjectA(_x86: &mut X86, _handle: u32, _bytes: u32, out: u32) -> u32 {
+    0 // fail
+}
+
 fn CreateCompatibleDC(x86: &mut X86, hdc: u32) -> u32 {
     assert!(hdc == 0); // null means "compatible with current screen"
     x86.state.gdi32.dcs.push(DC {});
@@ -25,5 +34,8 @@ fn CreateCompatibleDC(x86: &mut X86, hdc: u32) -> u32 {
 
 winapi!(
     fn GetStockObject(i: u32);
+    fn SelectObject(hdc: u32, hGdiObj: u32);
+    fn GetObjectA(handle: u32, bytes: u32, out: u32);
+
     fn CreateCompatibleDC(hdc: u32);
 );
