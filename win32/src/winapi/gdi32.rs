7dfc124d3e807553b1228dc6b39375c41c37bd35
Evan Martin <evan.martin@gmail.com>
2022-10-07 22:09:47 -0700

    SelectObject

--- win32/src/winapi/gdi32.rs
+++ win32/src/winapi/gdi32.rs
@@ -9,7 +9,14 @@ pub enum Object {
 }
 
 #[derive(Debug)]
-struct DC {}
+struct DC {
+    bitmap: u32, // HANDLE
+}
+impl DC {
+    fn new() -> Self {
+        DC { bitmap: 0 }
+    }
+}
 
 pub struct State {
     dcs: Vec<DC>,
@@ -23,9 +30,9 @@ impl State {
         }
     }
 
-    fn get_dc(&self, handle: u32) -> Option<&DC> {
+    fn get_dc(&mut self, handle: u32) -> Option<&mut DC> {
         if handle > 0 {
-            self.dcs.get((handle - 1) as usize)
+            self.dcs.get_mut((handle - 1) as usize)
         } else {
             None
         }
@@ -45,17 +52,21 @@ fn GetStockObject(_x86: &mut X86, _i: u32) -> u32 {
 }
 
 fn SelectObject(x86: &mut X86, hdc: u32, hGdiObj: u32) -> u32 {
-    let dc = match x86.state.gdi32.get_dc(hdc) {
-        None => return 0, // TODO: HGDI_ERROR
-        Some(dc) => dc,
-    };
     let obj = match x86.state.gdi32.get_object(hGdiObj) {
         None => return 0, // TODO: HGDI_ERROR
         Some(obj) => obj,
     };
-    log::warn!("SelectObject({:?}, {:?})", dc, obj);
-    // TODO: return previous obj
-    0
+
+    let op = match obj {
+        Object::Bitmap(_) => |dc: &mut DC| std::mem::replace(&mut dc.bitmap, hGdiObj),
+    };
+
+    let dc = match x86.state.gdi32.get_dc(hdc) {
+        None => return 0, // TODO: HGDI_ERROR
+        Some(dc) => dc,
+    };
+
+    op(dc) // returns previous value
 }
 
 fn GetObjectA(x86: &mut X86, handle: u32, _bytes: u32, _out: u32) -> u32 {
@@ -70,7 +81,7 @@ fn GetObjectA(x86: &mut X86, handle: u32, _bytes: u32, _out: u32) -> u32 {
 
 fn CreateCompatibleDC(x86: &mut X86, hdc: u32) -> u32 {
     assert!(hdc == 0); // null means "compatible with current screen"
-    x86.state.gdi32.dcs.push(DC {});
+    x86.state.gdi32.dcs.push(DC::new());
     x86.state.gdi32.dcs.len() as u32
 }
 
