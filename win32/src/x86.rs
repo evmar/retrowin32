6906d3ecfb00badbbcf2dab68d80fd94f9da355e
Evan Martin <evan.martin@gmail.com>
2023-01-04 16:35:03 -0800

    repatch instruction stream on desync

--- win32/src/x86.rs
+++ win32/src/x86.rs
@@ -764,18 +764,56 @@ impl Runner {
         code
     }
 
-    fn ip_to_instr_index(&self, target_ip: u32) -> Option<usize> {
+    /// Given an IP that wasn't found in the decoded instructions, re-decode starting at that
+    /// address and patch in the new instructions in place of the previous ones.
+    /// start is the index of where in the instruction table the patch will begin.
+    fn repatch(&mut self, addr: u32, start: usize) {
+        let mut decoder = iced_x86::Decoder::with_ip(
+            32,
+            &self.x86.mem[addr as usize..],
+            addr as u64,
+            iced_x86::DecoderOptions::NONE,
+        );
+        let mut end = start;
+        let mut patch = Vec::new();
+        while decoder.can_decode() {
+            let ip = decoder.ip() as u32;
+            // Overwrite any instructions with conflicting ips.
+            while ip > self.instrs[end].0 {
+                end += 1;
+            }
+            if ip == self.instrs[end].0 {
+                break;
+            }
+            patch.push((decoder.ip() as u32, decoder.decode()));
+            if end - start > 100 {
+                // We haven't hit this, just a defense against this going wrong.
+                panic!("resync: oversized patch?");
+            }
+        }
+        log::info!(
+            "replacing [{:x}..{:x}] with {} instrs starting at {:x}",
+            self.instrs[start].0,
+            self.instrs[end].0,
+            patch.len(),
+            patch[0].0,
+        );
+        self.instrs.splice(start..end, patch);
+    }
+
+    fn ip_to_instr_index(&mut self, target_ip: u32) -> Option<usize> {
         match self.instrs.binary_search_by_key(&target_ip, |&(ip, _)| ip) {
             Ok(pos) => Some(pos),
             Err(pos) => {
-                // I think we may hit this case if the disassembler gets desynchronized from the instruction
-                // stream.  We might need to re-disassemble or something in that case.
+                // We may hit this case if the disassembler gets desynchronized from the instruction
+                // stream.  We need to re-disassemble or something in that case.
                 let nearby = match self.instrs.get(pos) {
                     Some(&(ip, _)) => format!("nearby address {:x}", ip),
                     None => format!("address beyond decoded range"),
                 };
-                log::error!("invalid ip {:x}, possible desync? {}", target_ip, nearby);
-                None
+                log::error!("invalid ip {:x}, desync? {}", target_ip, nearby);
+                self.repatch(target_ip, pos);
+                self.ip_to_instr_index(target_ip)
             }
         }
     }
