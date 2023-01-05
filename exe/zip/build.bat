4a5292a4b2698509383a13bb7804b359f572ce22
Evan Martin <evan.martin@gmail.com>
2023-01-04 16:40:29 -0800

    more zip debug info

--- exe/zip/build.bat
+++ exe/zip/build.bat
@@ -3,8 +3,11 @@
 rem /std           c++ version
 rem /EHsc          allow exceptions
 rem /Os            optimize for size
+rem /GS-           disable security checks
 rem /link          linker options follow
 rem /subsystem     console binary
 rem /nologo        don't print copyright goop
+rem /debug         debug info
+rem /opt:ref       enable optimizations despite debug info
 
-cl zip.cc miniz.c /std:c++20 /EHsc /Os /link /subsystem:console /nologo kernel32.lib
+cl zip.cc miniz.c /std:c++20 /EHsc /Os /GS- /link /debug /opt:ref /subsystem:console /nologo kernel32.lib
