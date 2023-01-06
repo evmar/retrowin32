1d87d28f3e09cf05a8dc62d49927fe1edb358a63
Evan Martin <evan.martin@gmail.com>
2023-01-05 18:50:31 -0800

    disable aslr

--- exe/zip/build.bat
+++ exe/zip/build.bat
@@ -5,9 +5,10 @@ rem /EHsc          allow exceptions
 rem /Os            optimize for size
 rem /GS-           disable security checks
 rem /link          linker options follow
+rem /dynamicbase   ASLR
 rem /subsystem     console binary
 rem /nologo        don't print copyright goop
 rem /debug         debug info
 rem /opt:ref       enable optimizations despite debug info
 
-cl zip.cc miniz.c /std:c++20 /EHsc /Os /GS- /link /debug /opt:ref /subsystem:console /nologo kernel32.lib
+cl zip.cc miniz.c /std:c++20 /EHsc /Os /GS- /link /dynamicbase:no /debug /opt:ref /subsystem:console /nologo kernel32.lib
