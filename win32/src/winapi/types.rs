55e54e69837c394258db740ed40263b8ac936f73
Evan Martin <evan.martin@gmail.com>
2023-01-05 18:07:18 -0800

    comment

--- win32/src/winapi/types.rs
+++ win32/src/winapi/types.rs
@@ -1,8 +1,14 @@
+//! Types exposed by the Windows API.
+
 use super::shims::FromX86;
 
 pub type WORD = u16;
 pub type DWORD = u32;
 
+// Handles like HFILE etc. are just u32s.
+// I looked at using PhantomData to declare distinct types but I think using
+// a macro generates an equivalent amount of code and it's way less confusing
+// to work with.
 macro_rules! declare_handle {
     ($name:ident) => {
         #[derive(Debug, Eq, PartialEq)]
