b125dad6d4079161b92aebd0b30fefa0c2c59ab9
Evan Martin <evan.martin@gmail.com>
2023-05-12 15:55:13 -0700

    trace: avoid sse instructions

--- exe/trace/build.sh
+++ exe/trace/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-exec zig build-exe trace.zig -O ReleaseSmall -target x86-windows-msvc -fsingle-threaded
+# Use cpu=i686 here to avoid generating SSE instructions.
+
+exec zig build-exe trace.zig -mcpu=i686 --verbose-llvm-cpu-features -O ReleaseSmall -target x86-windows-msvc -fsingle-threaded
