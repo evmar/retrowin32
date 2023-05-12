fd7e7ec3c94520f00c3f8d3cad54dea5e9549782
Evan Martin <evan.martin@gmail.com>
2023-05-12 16:24:05 -0700

    trace: fwd build args

--- exe/trace/build.sh
+++ exe/trace/build.sh
@@ -2,4 +2,4 @@
 
 # Use cpu=i686 here to avoid generating SSE instructions.
 
-exec zig build-exe trace.zig -mcpu=i686 --verbose-llvm-cpu-features -O ReleaseSmall -target x86-windows-msvc -fsingle-threaded
+exec zig build-exe trace.zig -mcpu=i686 --verbose-llvm-cpu-features -O ReleaseSmall -target x86-windows-msvc -fsingle-threaded "$@"
