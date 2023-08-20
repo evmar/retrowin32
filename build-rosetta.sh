3d7466e741da99df1ebd4222b94d83b04f9b2165
Evan Martin <evan.martin@gmail.com>
2023-08-19 21:35:27 -0700

    rosetta: link sdl

--- build-rosetta.sh
+++ build-rosetta.sh
@@ -26,4 +26,4 @@ done
 # --print link-args
 export RUSTFLAGS="-C relocation-model=dynamic-no-pic $link_flag"
 
-exec cargo build --target x86_64-apple-darwin -p retrowin32 "$@"
+exec cargo build --target x86_64-apple-darwin -p retrowin32 --features sdl "$@"
