75f04b540fae7d4acbe30670c0021b87c6742df6
Evan Martin <evan.martin@gmail.com>
2024-03-08 11:39:14 -0800

    unneeded todo

--- win32/src/winapi/user32/mod.rs
+++ win32/src/winapi/user32/mod.rs
@@ -189,9 +189,6 @@ pub fn SetTimer(
     const USER_TIMER_MINIMUM: u32 = 0x0000_000A;
     const USER_TIMER_MAXIMUM: u32 = 0x7FFF_FFFF;
     let _uElapse = num_traits::clamp(uElapse, USER_TIMER_MINIMUM, USER_TIMER_MAXIMUM);
-    if lpTimerFunc != 0 {
-        todo!("SetTimer with callback");
-    }
 
     log::warn!("registering timer that will never fire");
     let timer = machine.state.user32.next_timer;
