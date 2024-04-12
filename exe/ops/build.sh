c18d6b12d19192d875f05448efd567f3d71c4612
Evan Martin <evan.martin@gmail.com>
2024-04-12 13:52:13 -0700

    pass (not working)? /Brepro

--- exe/ops/build.sh
+++ exe/ops/build.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-set -e
+set -ex
 
 export PATH=~/.local/opt/llvm/bin:$PATH
 
 clang_flags="-fuse-ld=lld -target i686-pc-windows-msvc"
-warning_flags="-Wall -Wno-nonportable-system-include-path -Wno-c++98-compat \
-    -Wno-unsafe-buffer-usage -Wno-old-style-cast -Wno-extra-semi-stmt -Wno-missing-prototypes"
-# optimize for size, generate debug info, no security cookies
-cflags="/Os /Zi /GS- /std:c++17 $warning_flags"
-xwin_path=~/redist/
+warning_flags="-Wall -Wno-nonportable-system-include-path -Wno-c++98-compat
+    -Wno-unsafe-buffer-usage -Wno-old-style-cast -Wno-missing-prototypes"
+# reproducible builds, optimize for size, generate debug info, no security cookies
+cflags="/Brepro /Os /Zi /GS- /std:c++17 $warning_flags"
+xwin_path=~/redist
 sdk_flags="/vctoolsdir $xwin_path/crt /winsdkdir $xwin_path/sdk"
 link_flags="/nodefaultlib kernel32.lib /subsystem:console"
 
