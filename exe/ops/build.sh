c8125daf1dcd7a62af033cefaa8571b1f87dee17
Evan Martin <evan.martin@gmail.com>
2024-04-13 14:37:16 -0700

    reproducible ops.exe

--- exe/ops/build.sh
+++ exe/ops/build.sh
@@ -7,8 +7,9 @@ export PATH=~/.local/opt/llvm/bin:$PATH
 clang_flags="-fuse-ld=lld -target i686-pc-windows-msvc"
 warning_flags="-Wall -Wno-nonportable-system-include-path -Wno-c++98-compat
     -Wno-unsafe-buffer-usage -Wno-old-style-cast -Wno-missing-prototypes"
-# reproducible builds, optimize for size, generate debug info, no security cookies
-cflags="/Brepro /Os /Zi /GS- /std:c++17 $warning_flags"
+# reproducible builds, optimize for size, no security cookies
+# note: /Zi for debug info (useful for ghidra) but it breaks build reproducibility
+cflags="/Brepro /Os /GS- /std:c++17 $warning_flags"
 xwin_path=~/redist
 sdk_flags="/vctoolsdir $xwin_path/crt /winsdkdir $xwin_path/sdk"
 link_flags="/nodefaultlib kernel32.lib /subsystem:console"
