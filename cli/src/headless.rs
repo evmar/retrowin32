2c207310865e45ddc3c84c6dcdc1bedaf2d0c85d
Evan Martin <evan.martin@gmail.com>
2024-04-13 15:16:11 -0700

    restore headless build

--- cli/src/headless.rs
+++ cli/src/headless.rs
@@ -4,12 +4,19 @@ impl GUI {
     pub fn new() -> anyhow::Result<Self> {
         Ok(GUI {})
     }
-    pub fn get_message(&self, _wait: bool) -> Option<win32::Message> {
+
+    pub fn time(&self) -> u32 {
+        0
+    }
+
+    pub fn get_message(&self, _wait: win32::Wait) -> Option<win32::Message> {
         unimplemented!();
     }
-    pub fn create_window(&mut self) -> Box<dyn win32::Window> {
+
+    pub fn create_window(&mut self, _hwnd: u32) -> Box<dyn win32::Window> {
         unimplemented!();
     }
+
     pub fn create_surface(&mut self, _opts: &win32::SurfaceOptions) -> Box<dyn win32::Surface> {
         unimplemented!();
     }
