8a852774d0acb7862a623bd3cc2f643c64e68e52
Evan Martin <evan.martin@gmail.com>
2024-04-21 09:21:56 -0700

    louder failure if missing env var

--- exe/gdi/build.rs
+++ exe/gdi/build.rs
@@ -1,6 +1,6 @@
 fn main() {
     println!("cargo:rerun-if-env-changed=XWIN");
-    let xwin = std::env::var("XWIN").unwrap_or_default();
+    let xwin = std::env::var("XWIN").unwrap();
     for dir in ["crt/lib/x86", "sdk/lib/ucrt/x86", "sdk/lib/um/x86"] {
         println!(r"cargo:rustc-link-search={xwin}/{dir}");
     }
