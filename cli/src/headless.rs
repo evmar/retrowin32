106dd51f7b2fbdc8d7827988bfecbc2e1cbd0adb
Evan Martin <evan.martin@gmail.com>
2024-05-28 07:30:55 -0700

    fix headless

--- cli/src/headless.rs
+++ cli/src/headless.rs
@@ -21,11 +21,7 @@ impl GUI {
         unimplemented!();
     }
 
-    pub fn create_surface(
-        &mut self,
-        _hwnd: u32,
-        _opts: &win32::SurfaceOptions,
-    ) -> Box<dyn win32::Surface> {
+    pub fn create_surface(&mut self, _opts: &win32::SurfaceOptions) -> Box<dyn win32::Surface> {
         unimplemented!();
     }
 }
