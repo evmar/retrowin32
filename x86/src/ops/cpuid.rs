75389145a971b890ae047b8453cb143dec3ffa4d
Evan Martin <evan.martin@gmail.com>
2024-05-28 08:05:52 -0700

    cpuid(eax=0), basic cpu info

--- x86/src/ops/cpuid.rs
+++ x86/src/ops/cpuid.rs
@@ -11,6 +11,14 @@ bitflags! {
 
 pub fn cpuid(cpu: &mut CPU, _mem: Mem, _instr: &Instruction) {
     match cpu.regs.get32(Register::EAX) {
+        0 => {
+            // basic information
+            cpu.regs.set32(Register::EAX, /* Pentium */ 0x2);
+            // "GenuineIntel"
+            cpu.regs.set32(Register::EBX, u32::from_le_bytes(*b"Genu"));
+            cpu.regs.set32(Register::EDX, u32::from_le_bytes(*b"ineI"));
+            cpu.regs.set32(Register::ECX, u32::from_le_bytes(*b"ntel"));
+        }
         1 => {
             // CPUID_GETFEATURES
             // Just enough to convince heaven7 that we support MMX.
