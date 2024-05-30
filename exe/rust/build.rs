a42030ba05e89a632da2c121b11238e5b82726a9
Evan Martin <evan.martin@gmail.com>
2024-05-30 12:17:42 -0700

    don't break analyzer when xwin not installed

--- exe/rust/build.rs
+++ exe/rust/build.rs
@@ -1,9 +1,10 @@
 #[cfg(target_family = "unix")]
 fn main() {
     println!("cargo:rerun-if-env-changed=XWIN");
-    let xwin = std::env::var("XWIN").unwrap();
-    for dir in ["crt/lib/x86", "sdk/lib/ucrt/x86", "sdk/lib/um/x86"] {
-        println!(r"cargo:rustc-link-search={xwin}/{dir}");
+    if let Ok(xwin) = std::env::var("XWIN") {
+        for dir in ["crt/lib/x86", "sdk/lib/ucrt/x86", "sdk/lib/um/x86"] {
+            println!(r"cargo:rustc-link-search={xwin}/{dir}");
+        }
     }
 }
 
