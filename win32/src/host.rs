a71fa8a15aacd6f6277ec503a58000c008c6a9c2
Evan Martin <evan.martin@gmail.com>
2024-05-30 13:04:15 -0700

    wasm build fix

--- win32/src/host.rs
+++ win32/src/host.rs
@@ -14,7 +14,7 @@ pub trait Surface {
     fn bit_blt(&mut self, dx: u32, dy: u32, src: &dyn Surface, sx: u32, sy: u32, w: u32, h: u32);
 }
 
-#[cfg_attr(target_family = "wasm", wasm_bindgen)]
+#[cfg_attr(target_family = "wasm", wasm_bindgen::prelude::wasm_bindgen)]
 #[derive(Clone, Debug)]
 pub struct SurfaceOptions {
     pub width: u32,
