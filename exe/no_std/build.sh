8f2eae0329227f19c45bb0b76087ffd80ac030c1
Evan Martin <evan.martin@gmail.com>
2024-06-02 14:09:50 -0700

    ensure correct dir

--- exe/no_std/build.sh
+++ exe/no_std/build.sh
@@ -3,5 +3,5 @@
 set -e
 
 # Note: keep as much as possible in the build.rs so that cargo-show-asm picks it up too.
-
+cd $(dirname $0)
 RUSTFLAGS='-C panic=abort' exec cargo build --target i586-pc-windows-msvc "$@"
