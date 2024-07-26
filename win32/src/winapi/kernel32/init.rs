f3ee19888761852479e159d122c60232515c4b96
Luke Street <luke@street.dev>
2024-07-25 18:07:38 -0600

    win32: Bump heap size to 24MB

--- win32/src/winapi/kernel32/init.rs
+++ win32/src/winapi/kernel32/init.rs
@@ -313,7 +313,7 @@ impl State {
 
     pub fn get_process_heap<'a>(&'a mut self, memory: &mut MemImpl) -> &mut Heap {
         if self.process_heap == 0 {
-            let size = 16 << 20;
+            let size = 24 << 20;
             let heap = self.new_heap(memory, size, "process heap".into());
             self.process_heap = heap;
         }
