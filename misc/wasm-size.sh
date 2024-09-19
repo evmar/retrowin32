168d339fca03601e3186f8161d13837d1a7e9e65
Evan Martin <evan.martin@gmail.com>
2024-09-19 09:12:22 -0700

    comments

--- misc/wasm-size.sh
+++ misc/wasm-size.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+# Builds and prints the size of the wasm bundle to stdout.
+# For use in https://github.com/evmar/git-metrics
+
 set -e
 
 rm -f glue/pkg/glue_bg.wasm
 if [ -f web/Makefile ]; then
     (make -C web profile=lto) 1>&2
 else
+    # In older versions, Makefile was at the root of the project.
     (make wasm opt=1) 1>&2
 fi
 
