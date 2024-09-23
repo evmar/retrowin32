130a8d4267585b820d48d6ac50c88a6d92315810
Evan Martin <evan.martin@gmail.com>
2024-09-23 13:30:09 -0700

    split rectangle functions module

--- win32/src/winapi/user32/misc.rs
+++ win32/src/winapi/user32/misc.rs
@@ -1,9 +1,6 @@
 use crate::{
     str16::Str16,
-    winapi::{
-        stack_args::VarArgs,
-        types::{HWND, POINT, RECT},
-    },
+    winapi::{stack_args::VarArgs, types::HWND},
     Machine,
 };
 use memory::{Extensions, ExtensionsMut};
@@ -48,32 +45,6 @@ pub fn GetSystemMetrics(_machine: &mut Machine, nIndex: Result<SystemMetric, u32
         SystemMetric::CXVIRTUALSCREEN => 640,
         SystemMetric::CYVIRTUALSCREEN => 480,
     }
-}
-
-#[win32_derive::dllexport]
-pub fn SetRect(
-    _machine: &mut Machine,
-    lprc: Option<&mut RECT>,
-    xLeft: i32,
-    yTop: i32,
-    xRight: i32,
-    yBottom: i32,
-) -> bool {
-    let rect = lprc.unwrap();
-    *rect = RECT {
-        left: xLeft,
-        top: yTop,
-        right: xRight,
-        bottom: yBottom,
-    };
-    true
-}
-
-#[win32_derive::dllexport]
-pub fn PtInRect(_machine: &mut Machine, lprc: Option<&RECT>, pt: POINT) -> bool {
-    let rect = lprc.unwrap();
-    let (x, y) = (pt.x as i32, pt.y as i32);
-    x >= rect.left && x < rect.right && y >= rect.top && y < rect.bottom
 }
 
 #[win32_derive::dllexport(cdecl)]
@@ -161,54 +132,6 @@ pub fn GetKeyState(_machine: &mut Machine, nVirtKey: u32) -> u32 {
 #[win32_derive::dllexport]
 pub fn IsIconic(_machine: &mut Machine, hwnd: HWND) -> bool {
     false
-}
-
-#[win32_derive::dllexport]
-pub fn IsRectEmpty(_machine: &mut Machine, lprc: Option<&RECT>) -> bool {
-    let rect = lprc.unwrap();
-    rect.left >= rect.right || rect.top >= rect.bottom
-}
-
-#[win32_derive::dllexport]
-pub fn SetRectEmpty(_machine: &mut Machine, lprc: Option<&mut RECT>) -> bool {
-    if lprc.is_none() {
-        return false;
-    }
-    let rect = lprc.unwrap();
-    rect.left = 0;
-    rect.top = 0;
-    rect.right = 0;
-    rect.bottom = 0;
-    true
-}
-
-#[win32_derive::dllexport]
-pub fn IntersectRect(
-    _machine: &mut Machine,
-    lprcDst: Option<&mut RECT>,
-    lprcSrc1: Option<&RECT>,
-    lprcSrc2: Option<&RECT>,
-) -> bool {
-    if lprcDst.is_none() || lprcSrc1.is_none() || lprcSrc2.is_none() {
-        return false;
-    }
-    let dst = lprcDst.unwrap();
-    let src1 = lprcSrc1.unwrap();
-    let src2 = lprcSrc2.unwrap();
-    if IsRectEmpty(_machine, lprcSrc1)
-        || IsRectEmpty(_machine, lprcSrc2)
-        || src1.left >= src2.right
-        || src1.right <= src2.left
-        || src1.top >= src2.bottom
-        || src1.bottom <= src2.top
-    {
-        return false;
-    }
-    dst.left = src1.left.max(src2.left);
-    dst.right = src1.right.min(src2.right);
-    dst.top = src1.top.max(src2.top);
-    dst.bottom = src1.bottom.min(src2.bottom);
-    dst.left < dst.right && dst.top < dst.bottom
 }
 
 #[win32_derive::dllexport]
---
+++ win32/src/winapi/user32/rect.rs
@@ -0,0 +1,80 @@
+use crate::{
+    winapi::types::{POINT, RECT},
+    Machine,
+};
+
+const TRACE_CONTEXT: &'static str = "user32/rect";
+
+#[win32_derive::dllexport]
+pub fn SetRect(
+    _machine: &mut Machine,
+    lprc: Option<&mut RECT>,
+    xLeft: i32,
+    yTop: i32,
+    xRight: i32,
+    yBottom: i32,
+) -> bool {
+    let rect = lprc.unwrap();
+    *rect = RECT {
+        left: xLeft,
+        top: yTop,
+        right: xRight,
+        bottom: yBottom,
+    };
+    true
+}
+
+#[win32_derive::dllexport]
+pub fn PtInRect(_machine: &mut Machine, lprc: Option<&RECT>, pt: POINT) -> bool {
+    let rect = lprc.unwrap();
+    let (x, y) = (pt.x as i32, pt.y as i32);
+    x >= rect.left && x < rect.right && y >= rect.top && y < rect.bottom
+}
+
+#[win32_derive::dllexport]
+pub fn IsRectEmpty(_machine: &mut Machine, lprc: Option<&RECT>) -> bool {
+    let rect = lprc.unwrap();
+    rect.left >= rect.right || rect.top >= rect.bottom
+}
+
+#[win32_derive::dllexport]
+pub fn SetRectEmpty(_machine: &mut Machine, lprc: Option<&mut RECT>) -> bool {
+    if lprc.is_none() {
+        return false;
+    }
+    let rect = lprc.unwrap();
+    rect.left = 0;
+    rect.top = 0;
+    rect.right = 0;
+    rect.bottom = 0;
+    true
+}
+
+#[win32_derive::dllexport]
+pub fn IntersectRect(
+    _machine: &mut Machine,
+    lprcDst: Option<&mut RECT>,
+    lprcSrc1: Option<&RECT>,
+    lprcSrc2: Option<&RECT>,
+) -> bool {
+    if lprcDst.is_none() || lprcSrc1.is_none() || lprcSrc2.is_none() {
+        return false;
+    }
+    let dst = lprcDst.unwrap();
+    let src1 = lprcSrc1.unwrap();
+    let src2 = lprcSrc2.unwrap();
+    if IsRectEmpty(_machine, lprcSrc1)
+        || IsRectEmpty(_machine, lprcSrc2)
+        || src1.left >= src2.right
+        || src1.right <= src2.left
+        || src1.top >= src2.bottom
+        || src1.bottom <= src2.top
+    {
+        return false;
+    }
+    dst.left = src1.left.max(src2.left);
+    dst.right = src1.right.min(src2.right);
+    dst.top = src1.top.max(src2.top);
+    dst.bottom = src1.bottom.min(src2.bottom);
+    dst.left < dst.right && dst.top < dst.bottom
+}
