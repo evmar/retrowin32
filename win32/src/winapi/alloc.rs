4f9116df54529049a75d7170530a4fb7c95b6648
Evan Martin <evan.martin@gmail.com>
2024-10-15 13:31:16 -0700

    kernel32: assert arena is aligned

--- win32/src/winapi/alloc.rs
+++ win32/src/winapi/alloc.rs
@@ -13,11 +13,13 @@ pub fn align_to(n: u32, align: usize) -> u32 {
 pub struct Arena {
     pub addr: u32,
     pub size: u32,
+    /// Offset within the arena for the next allocation.
     next: u32,
 }
 
 impl Arena {
     pub fn new(addr: u32, size: u32) -> Self {
+        assert!(addr == align_to(addr, 8));
         Arena {
             addr,
             size,
