2d9430b96368f8b467fe5fd3e2bb80a48804ae66
Evan Martin <evan.martin@gmail.com>
2025-01-31 20:21:29 -0800

    ucrtbase: memcpy

--- win32/src/winapi/ucrtbase/misc.rs
+++ win32/src/winapi/ucrtbase/misc.rs
@@ -16,3 +16,9 @@ pub fn strlen(_machine: &mut Machine, lpString: Option<&CStr>) -> u32 {
     // The mapping to str already computes the string length.
     lpString.unwrap().count_bytes() as u32
 }
+
+#[win32_derive::dllexport(cdecl)]
+pub fn memcpy(machine: &mut Machine, dest: u32, src: u32, count: u32) -> u32 {
+    machine.mem().copy(src, dest, count);
+    dest
+}
