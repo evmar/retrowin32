dbb0230d6d0271e2232b6100ebea4e84c8ccda9d
Evan Martin <evan.martin@gmail.com>
2025-02-06 14:29:10 -0800

    web: merge Emulator and Host

--- web/emulator.ts
+++ web/emulator.ts
@@ -1,18 +1,220 @@
 import { Breakpoints } from './debugger/break';
 import * as wasm from './glue/pkg/glue';
-import { Status as EmulatorStatus } from './glue/pkg/glue';
-import { fetchFileSet, FileSet, JsHost } from './host';
+import { Status } from './glue/pkg/glue';
+export { Status as EmulatorStatus };
 
-export { EmulatorStatus };
+async function fetchBytes(path: string): Promise<Uint8Array> {
+  const resp = await fetch(path);
+  if (!resp.ok) throw new Error(`failed to load ${path}`);
+  return new Uint8Array(await resp.arrayBuffer());
+}
+
+/** A set of (pre)loaded files; a temporary hack until the emulator can load files itself. */
+export type FileSet = Map<string, Uint8Array>;
+
+export async function fetchFileSet(files: string[], dir: string = ''): Promise<FileSet> {
+  const fileset: FileSet = new Map();
+  for (const file of files) {
+    const path = dir + file;
+    fileset.set(file, await fetchBytes(path));
+  }
+  return fileset;
+}
+
+class Window implements wasm.JsWindow {
+  constructor(readonly host: EmulatorHost, readonly hwnd: number) {
+    const stashEvent = (ev: Event) => {
+      (ev as any).hwnd = hwnd;
+      host.onEvent(ev);
+      return false;
+    };
+    this.canvas.onmousedown = stashEvent;
+    this.canvas.onmouseup = stashEvent;
+    this.canvas.onmousemove = stashEvent;
+    this.canvas.oncontextmenu = (ev) => {
+      return false;
+    };
+  }
+
+  title: string = '';
+  canvas: HTMLCanvasElement = document.createElement('canvas');
+  size: [number, number] = [0, 0];
+  is_fullscreen = false;
+
+  private reset_canvas() {
+    const [w, h] = this.size;
+    let scale = 1;
+    if (this.is_fullscreen && w < 640) scale *= 2;
+
+    // Note: the canvas must be sized to the size of physical pixels,
+    // or else it will be scaled up with smoothing and pixels will be blurry.
+    this.canvas.width = w * window.devicePixelRatio;
+    this.canvas.height = h * window.devicePixelRatio;
+    this.canvas.style.width = `${w * scale}px`;
+    this.canvas.style.height = `${h * scale}px`;
+
+    // The context scale seems preserved across calls to getContext, but then also
+    // lost when the canvas is resized.  Rather than relying on this, always reset
+    // and scale the context immediately on resize.
+    const ctx = this.canvas.getContext('2d')!;
+    ctx.reset();
+    ctx.imageSmoothingEnabled = false;
+    ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
+    this.host.onWindowChanged();
+  }
+
+  set_size(w: number, h: number) {
+    this.size = [w, h];
+    this.reset_canvas();
+  }
+
+  fullscreen() {
+    this.is_fullscreen = true;
+    this.reset_canvas();
+  }
+}
+
+class File implements wasm.JsFile {
+  ofs = 0;
+
+  constructor(readonly path: string, readonly bytes: Uint8Array) {
+  }
+
+  info(): number {
+    return this.bytes.length;
+  }
+
+  seek(from: number, ofs: number): number {
+    switch (from) {
+      case 0: // start
+        break;
+      case 1: // end
+        ofs = this.bytes.length + ofs;
+        break;
+      case 2: // cur
+        ofs = this.ofs + ofs;
+        break;
+    }
+    if (ofs < 0 || ofs > this.bytes.length) {
+      throw new Error(`seek out of bounds: ${from}:${ofs}`);
+    }
+    this.ofs = ofs;
+    return ofs;
+  }
+
+  read(buf: Uint8Array): number {
+    const n = Math.min(buf.length, this.bytes.length - this.ofs);
+    buf.set(this.bytes.subarray(this.ofs, this.ofs + n));
+    this.ofs += n;
+    return n;
+  }
+
+  write(buf: Uint8Array): number {
+    console.warn('ignoring write');
+    return buf.length;
+  }
+}
+
 
 /** Functions the emulator may need to call. */
 export interface EmulatorHost {
   onWindowChanged(): void;
   onError(msg: string): void;
   onStdOut(stdout: string): void;
-  onStopped(status: EmulatorStatus): void;
+  onStopped(status: Status): void;
   /** DOM event on an emulator surface; should be forwarded to emulator. */
   onEvent(event: Event): void;
+}
+
+/** Emulator host, providing the emulation=>web API.  Extended by Emulator class. */
+export abstract class JsHost implements wasm.JsHost, wasm.JsLogger {
+  // TODO: fold these classes together
+  protected events: Event[] = [];
+  private timer?: number;
+
+  decoder = new TextDecoder();
+
+  constructor(readonly emuHost: EmulatorHost, readonly files: FileSet) { }
+
+  log(level: number, msg: string) {
+    // TODO: surface this in the UI.
+    switch (level) {
+      case 1:
+        console.error(msg);
+        this.emuHost.onError(msg);
+        break;
+      case 2:
+        console.warn(msg);
+        break;
+      case 3:
+        console.info(msg);
+        break;
+      case 4:
+      case 5: // "trace"
+        console.debug(msg);
+        break;
+      default:
+        throw new Error(`unexpected log level #{level}`);
+    }
+  }
+
+  abstract start(): void;
+
+  ensure_timer(when: number): void {
+    if (this.timer) {
+      clearTimeout(this.timer);
+    }
+    const now = performance.now();
+    console.log('timer for', when - now);
+    const id = setTimeout(() => {
+      if (this.timer !== id) {
+        return;
+      }
+      this.timer = undefined;
+      this.start();
+    }, when - now);
+    this.timer = id;
+  }
+
+  get_event(): Event | undefined {
+    return this.events.shift();
+  }
+
+  open(path: string, options: wasm.FileOptions): wasm.JsFile | null {
+    if (options.create) {
+      return new File(path, new Uint8Array());
+    }
+    // TODO: async file loading.
+    let bytes = this.files.get(path);
+    if (!bytes) {
+      return null;
+    }
+    return new File(path, bytes);
+  }
+
+  stdout(buf: Uint8Array) {
+    const text = this.decoder.decode(buf);
+    this.emuHost.onStdOut(text);
+  }
+
+  windows: Window[] = [];
+  create_window(hwnd: number): wasm.JsWindow {
+    let window = new Window(this.emuHost, hwnd);
+    this.windows.push(window);
+    this.emuHost.onWindowChanged();
+    return window;
+  }
+
+  screen() {
+    // XXX how to tie surface and window together?
+    // The DirectDraw calls SetCooperativeLevel() on the hwnd, and then CreateSurface with primary,
+    // but how to plumb that info across JS boundary?
+    return this.windows[this.windows.length - 1].canvas.getContext('2d')!;
+  }
+
+  audio(buf: Int16Array) {
+    console.warn('TODO: audio');
+  }
 }
 
 /** Wraps wasm.Emulator, able to run in a loop while still yielding to browser events. */
