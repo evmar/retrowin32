978f0b2eb0bab9fa2d4aff420a680210b5fa31d7
Evan Martin <evan.martin@gmail.com>
2025-03-06 10:40:50 -0800

    winapi: move printf into shared implementation

---
+++ win32/src/winapi/printf.rs
@@ -0,0 +1,70 @@
+use crate::calling_convention::VarArgs;
+use memory::{Extensions, Mem};
+
+/// An implementation of the C `printf` function, used in DLLs that need one;
+/// both user32 and msvcrt expose variations.
+pub fn printf(
+    out: &mut dyn std::io::Write,
+    fmt: &str,
+    mut args: VarArgs,
+    mem: Mem,
+) -> anyhow::Result<()> {
+    fn read_number(c: u8) -> usize {
+        // TODO: multiple digits, error handling, etc.
+        assert!(c >= b'0' && c <= b'9');
+        (c - b'0') as usize
+    }
+
+    let mut i = fmt.bytes();
+    while let Some(c) = i.next() {
+        if c == b'%' {
+            let mut c = i.next().unwrap();
+
+            let mut width = 0;
+            if c >= b'0' && c <= b'9' {
+                width = read_number(c);
+                c = i.next().unwrap();
+            }
+
+            let mut precision = 0;
+            if c == b'.' {
+                c = i.next().unwrap();
+                precision = read_number(c);
+                c = i.next().unwrap();
+            }
+
+            let mut long = false;
+            if c == b'l' {
+                long = true;
+                c = i.next().unwrap();
+            }
+            _ = long; // currently ignored
+
+            match c {
+                b'u' => write!(
+                    out,
+                    "{:width$.precision$}",
+                    args.pop::<u32>(mem),
+                    width = width,
+                    precision = precision
+                )?,
+                b'd' => write!(
+                    out,
+                    "{:width$.precision$}",
+                    args.pop::<i32>(mem),
+                    width = width,
+                    precision = precision
+                )?,
+                b's' => {
+                    let addr = args.pop::<u32>(mem);
+                    let str = mem.slicez(addr);
+                    write!(out, "{}", std::str::from_utf8(str)?)?;
+                }
+                _ => todo!("format string character {:?}", c as char),
+            }
+        } else {
+            out.write(&[c])?;
+        }
+    }
+    Ok(())
+}
