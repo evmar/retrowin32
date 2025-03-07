4730e2843f87bb26da3d01643fd77f94e811d225
Evan Martin <evan.martin@gmail.com>
2025-03-06 19:22:23 -0800

    test ddraw executable

---
+++ exe/rust/ddraw-sys/src/lib.rs
@@ -0,0 +1,12 @@
+//! Bindings to the DirectDraw API.
+
+// Wrap and re-export the bindgen module so we can suppress warnings.
+
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(unused)]
+// Help rust-analyzer ignore that we're using stdcall:
+#![allow(unsupported_fn_ptr_calling_conventions)]
+
+mod bindgen;
+pub use bindgen::*;
