0e715a18c03a3028ddf37cf67c8e715e5a883902
Evan Martin <evan.martin@gmail.com>
2025-03-30 11:03:55 -0700

    kernel32: GetModuleFileNameW, with some string generalization

--- win32/src/winapi/string.rs
+++ win32/src/winapi/string.rs
@@ -59,16 +59,26 @@ fn write_wide(buf: &mut [u8], str: &str) -> Result<u32, u32> {
 
 /// BufWrite and its impls let code be generic over the output buffer type.
 pub trait BufWrite<'a> {
+    /// Get the size of the buffer, in characters (not the number of bytes written).
+    fn capacity(&self) -> u32;
+    /// Return the number of characters written.
+    /// Err(n) is the number of characters needed when buffer is too small.
     fn write(&mut self, str: &str) -> Result<u32, u32>;
 }
 
 pub struct BufWriteAnsi<'a>(&'a mut [u8]); // TODO: codepage
 impl<'a> BufWriteAnsi<'a> {
-    pub fn new(mem: Mem<'a>, ofs: u32, count: u32) -> Self {
+    pub fn new(buf: &'a mut [u8]) -> Self {
+        Self(buf)
+    }
+    pub fn from_mem(mem: Mem<'a>, ofs: u32, count: u32) -> Self {
         Self(mem.sub32_mut(ofs, count))
     }
 }
 impl<'a> BufWrite<'a> for BufWriteAnsi<'a> {
+    fn capacity(&self) -> u32 {
+        self.0.len() as u32
+    }
     fn write(&mut self, str: &str) -> Result<u32, u32> {
         write_ansi(self.0, str)
     }
@@ -76,11 +86,17 @@ impl<'a> BufWrite<'a> for BufWriteAnsi<'a> {
 
 pub struct BufWriteWide<'a>(&'a mut [u8]);
 impl<'a> BufWriteWide<'a> {
-    pub fn new(mem: Mem<'a>, ofs: u32, count: u32) -> Self {
-        Self(mem.sub32_mut(ofs, count * 2))
+    pub fn new(buf: &'a mut [u8]) -> Self {
+        Self(buf)
+    }
+    pub fn from_mem(mem: Mem<'a>, ofs: u32, count: u32) -> Self {
+        Self::new(mem.sub32_mut(ofs, count * 2))
     }
 }
 impl<'a> BufWrite<'a> for BufWriteWide<'a> {
+    fn capacity(&self) -> u32 {
+        self.0.len() as u32 / 2
+    }
     fn write(&mut self, str: &str) -> Result<u32, u32> {
         write_wide(self.0, str)
     }
