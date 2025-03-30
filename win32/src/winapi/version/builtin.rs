1df798511ae7c4041a009afb0186e8a62167efc2
Evan Martin <evan.martin@gmail.com>
2025-03-29 21:49:27 -0700

    version.dll stubs

--- win32/src/winapi/version/builtin.rs
+++ win32/src/winapi/version/builtin.rs
@@ -13,6 +13,35 @@ mod wrappers {
     };
     use ::memory::Extensions;
     use winapi::version::*;
+    pub unsafe fn GetFileVersionInfoA(machine: &mut Machine, stack_args: u32) -> ABIReturn {
+        let mem = machine.mem().detach();
+        let lptstrFilename = <Option<&str>>::from_stack(mem, stack_args + 0u32);
+        let dwHandle = <u32>::from_stack(mem, stack_args + 4u32);
+        let dwLen = <u32>::from_stack(mem, stack_args + 8u32);
+        let lpData = <u32>::from_stack(mem, stack_args + 12u32);
+        let __trace_record = if crate::winapi::trace::enabled("version") {
+            crate::winapi::trace::Record::new(
+                winapi::version::GetFileVersionInfoA_pos,
+                "version",
+                "GetFileVersionInfoA",
+                &[
+                    ("lptstrFilename", &lptstrFilename),
+                    ("dwHandle", &dwHandle),
+                    ("dwLen", &dwLen),
+                    ("lpData", &lpData),
+                ],
+            )
+            .enter()
+        } else {
+            None
+        };
+        let result =
+            winapi::version::GetFileVersionInfoA(machine, lptstrFilename, dwHandle, dwLen, lpData);
+        if let Some(mut __trace_record) = __trace_record {
+            __trace_record.exit(&result);
+        }
+        result.into()
+    }
     pub unsafe fn GetFileVersionInfoSizeA(machine: &mut Machine, stack_args: u32) -> ABIReturn {
         let mem = machine.mem().detach();
         let lptstrFilename = <Option<&str>>::from_stack(mem, stack_args + 0u32);
@@ -37,11 +66,50 @@ mod wrappers {
         }
         result.into()
     }
+    pub unsafe fn VerQueryValueA(machine: &mut Machine, stack_args: u32) -> ABIReturn {
+        let mem = machine.mem().detach();
+        let pBlock = <u32>::from_stack(mem, stack_args + 0u32);
+        let lpSubBlock = <Option<&str>>::from_stack(mem, stack_args + 4u32);
+        let lplpBuffer = <u32>::from_stack(mem, stack_args + 8u32);
+        let puLen = <Option<&mut u32>>::from_stack(mem, stack_args + 12u32);
+        let __trace_record = if crate::winapi::trace::enabled("version") {
+            crate::winapi::trace::Record::new(
+                winapi::version::VerQueryValueA_pos,
+                "version",
+                "VerQueryValueA",
+                &[
+                    ("pBlock", &pBlock),
+                    ("lpSubBlock", &lpSubBlock),
+                    ("lplpBuffer", &lplpBuffer),
+                    ("puLen", &puLen),
+                ],
+            )
+            .enter()
+        } else {
+            None
+        };
+        let result =
+            winapi::version::VerQueryValueA(machine, pBlock, lpSubBlock, lplpBuffer, puLen);
+        if let Some(mut __trace_record) = __trace_record {
+            __trace_record.exit(&result);
+        }
+        result.into()
+    }
 }
-const SHIMS: [Shim; 1usize] = [Shim {
-    name: "GetFileVersionInfoSizeA",
-    func: Handler::Sync(wrappers::GetFileVersionInfoSizeA),
-}];
+const SHIMS: [Shim; 3usize] = [
+    Shim {
+        name: "GetFileVersionInfoA",
+        func: Handler::Sync(wrappers::GetFileVersionInfoA),
+    },
+    Shim {
+        name: "GetFileVersionInfoSizeA",
+        func: Handler::Sync(wrappers::GetFileVersionInfoSizeA),
+    },
+    Shim {
+        name: "VerQueryValueA",
+        func: Handler::Sync(wrappers::VerQueryValueA),
+    },
+];
 pub const DLL: BuiltinDLL = BuiltinDLL {
     file_name: "version.dll",
     shims: &SHIMS,
