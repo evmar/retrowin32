f01f155558a624e0214513b1ca0fb51973c7f5d0
Evan Martin <evan.martin@gmail.com>
2025-04-03 14:40:29 -0700

    no_std: better panics, split helper mods

--- exe/no_std/src/lib.rs
+++ exe/no_std/src/lib.rs
@@ -1,19 +1,6 @@
 #![no_std]
 
+mod compiler_builtins;
 pub mod fpu;
+mod panic;
 pub mod print;
-
-#[cfg(not(test))]
-#[panic_handler]
-unsafe fn handle_panic(_: &core::panic::PanicInfo) -> ! {
-    print::print(b"panicked");
-    windows_sys::Win32::System::Threading::ExitProcess(1);
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn memcpy(dest: *mut u8, src: *const u8, n: usize) -> *mut u8 {
-    for i in 0..n {
-        *dest.add(i) = *src.add(i);
-    }
-    dest
-}
