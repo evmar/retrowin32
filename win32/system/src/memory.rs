a430d074a34828b68ddf772ded80c69881ec3396
Evan Martin <evan.martin@gmail.com>
2025-05-30 08:02:30 -0700

    memory alloc helper, better mappings dump

--- win32/system/src/memory.rs
+++ win32/system/src/memory.rs
@@ -143,11 +143,15 @@ impl Mappings {
     pub fn dump(&self) {
         for map in &self.0 {
             println!(
-                "{:08x}-{:08x} {:?} {:?}",
-                map.addr,
-                map.addr + map.size,
-                map.desc,
-                map.flags
+                "{addr:08x}-{size:08x} {module}{desc:?} {flags:?}",
+                addr = map.addr,
+                size = map.addr + map.size,
+                module = match &map.module {
+                    Some(module) => format!("{module}:"),
+                    None => "".into(),
+                },
+                desc = map.desc,
+                flags = map.flags
             );
         }
     }
@@ -210,13 +214,16 @@ impl Memory {
         heap
     }
 
+    pub fn alloc(&self, size: u32) -> u32 {
+        self.process_heap.alloc(self.mem(), size)
+    }
+
     /// Store some Pod data in the process heap.
     pub fn store<T: memory::Pod>(&self, data: T) -> u32 {
         use memory::ExtensionsMut;
-        let size = std::mem::size_of::<T>();
-        let mem = self.mem();
-        let addr = self.process_heap.alloc(mem, size as u32);
-        mem.put_pod::<T>(addr, data);
+        let size = std::mem::size_of::<T>() as u32;
+        let addr = self.alloc(size);
+        self.mem().put_pod::<T>(addr, data);
         addr
     }
 }
