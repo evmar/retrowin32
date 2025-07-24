94bd0aa95dd15226d7bfc85cf77e6156ea120ee1
Evan Martin <evan.martin@gmail.com>
2025-07-24 10:37:04 -0700

    don't include broken demos

--- appdb/run.sh
+++ appdb/run.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+cd "$(dirname "$0")"
+
 go run . -tmpl ../web/index.tmpl render > ../web/index.html
 go run . -tmpl ../web/index.tmpl -broken render > ../web/broken.html
 go run . deploy
