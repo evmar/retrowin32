baad4024058161bf0e759dee396c050d76546844
Linus Unnebäck <linus@folkdatorn.se>
2025-11-19 00:03:18 +0100

    Implement InterlockedDecrement

--- win32/dll/kernel32/src/sync/interlocked.rs
+++ win32/dll/kernel32/src/sync/interlocked.rs
@@ -9,5 +9,7 @@ pub fn InterlockedIncrement(sys: &dyn System, addend: Option<&mut u32>) -> u32 {
 
 #[win32_derive::dllexport]
 pub fn InterlockedDecrement(sys: &dyn System, addend: Option<&mut u32>) -> u32 {
-    todo!()
+    let addend = addend.unwrap();
+    *addend -= 1;
+    *addend
 }
